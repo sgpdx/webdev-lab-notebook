314617ca4302d886e0de7f753a7e31da82208b02
Serena <sglick@pdx.edu>
2025-11-10 16:44:50 -0800

    JavaScript Ex 06 Complete

--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -1,9 +1,69 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+// Code updated by Serena Glick / sglick@pdx / sgpdx@github
 
-const app = document.querySelector('#books');
+const url = "https://anapioficeandfire.com/api/books/";
+
+const app = document.querySelector("#books");
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
-  // Create an element for each book that contains title, author, publication year, and number of pages
-  // Update the styles in JavaScript to center all the books in the container given
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      data.forEach((book) => {
+        console.log(
+          book.name,
+          book.authors[0],
+          book.numberOfPages,
+          book.released
+        );
+        addBooktoDOM(book);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+
+      let element = document.createElement("div");
+      element.textContent = "An error occurred.  Please reload the page.";
+      app.appendChild(element);
+    })
+    .finally(() => {
+      let loader = document.querySelector("#loading");
+      app.removeChild(loader);
+    });
 };
+
+// Create an element for each book that contains title, author, publication year, and number of pages
+
+const addBooktoDOM = (book) => {
+  let element = document.createElement("div");
+  let title = document.createElement("h3");
+  let author = document.createElement("p");
+  let released = document.createElement("p");
+  let pages = document.createElement("p");
+
+  title.textContent = book.name;
+  author.textContent = book.authors[0];
+  released.textContent = book.released.substring(0, 4);
+  pages.textContent = `${book.numberOfPages} pages`;
+
+  element.appendChild(title);
+  element.appendChild(author);
+  element.appendChild(released);
+  element.appendChild(pages);
+
+  app.appendChild(element);
+
+  // Update the styles in JavaScript to center all the books in the container given
+  // Note: already centered via bootstrap code
+  // Additional styling done via elements <h3> and <p>
+  // However, to be explicit:
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
+};
+
+// Running the main function that calls the API
+fetchData(url);
